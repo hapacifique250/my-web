99250056562bef5f664d523dd8044a1948b3c58a
unknown <paccyhabi@gmail.com>
2025-12-03 18:40:06 +0200

    Add /about route to web application

--- index.js
+++ index.js
@@ -66,6 +66,10 @@ app.get('/profile', (req, res) => {
   }
 });
 
+app.get('/about', (req, res) => {
+  res.send('About page')
+}
+)
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
